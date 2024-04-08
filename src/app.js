4af05a48b36be6e2cf70992c5f0f4ff5e18adc28
AnggiAnggianaSujana <anggiana0092@gmail.com>
2024-04-08 22:52:59 +0700

    feat: add the function of 'cart_shop' features (add the product into cart, adding and subtracting the number of products) with javascript.

--- src/app.js
+++ src/app.js
@@ -10,6 +10,60 @@ document.addEventListener('alpine:init', () => {
             { id: 4, name: 'Mocha', img: 'Mocha.jpg', price: 27500 },
         ],
     }));
+
+    // Integrate 'Menu Product' with 'Cart' feature by the cart button
+    Alpine.store('cart', {
+        items: [],
+        quantity: 0,
+        totalPrice: 0,
+        // Method to add new item
+        add(newItem) {
+            // Check if the product already exists in cart
+            const cartItem = this.items.find((item) => item.id === newItem.id);
+
+            // IF the product didn't exists
+            if(!cartItem) {
+                this.items.push({...newItem, quantity: 1, totalPrice: newItem.price });
+                this.quantity++;
+                this.totalPrice += newItem.price;    
+            } else {
+                // If the product already exists
+                this.items = this.items.map((item) => {
+                    // If the product added is different with the product already exists
+                    if (item.id !== newItem.id) {
+                        return item;
+                    } else {
+                        // If the product is similar with the product already exists
+                        item.quantity++;
+                        item.totalPrice = item.price * item.quantity;
+                        this.quantity++;
+                        this.totalPrice += item.price;
+                        return item;
+                    }
+                })
+            }
+        },
+        remove(id) {
+            const cartItem = this.items.find((item) => item.id === id);
+
+            // IF item.quantity more than 1
+            if(cartItem.quantity > 1) {
+                // Check one by one
+                this.items = this.items.map((item) => {
+                    // If the id target didn't clicked
+                    if(item.id !== id) {
+                        return item;
+                    } else {
+                        item.quantity--;
+                        item.totalPrice = item.price * item.quantity;
+                        this.quantity--;
+                        this.totalPrice -= item.price;
+                        return item;
+                    }
+                })
+            }
+        }
+    })
 });
 
 // Convert price into Rupiah format
